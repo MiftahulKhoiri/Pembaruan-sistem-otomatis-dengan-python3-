d5a86b2cf206150af38beba8fd10739a7d27ebe9
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-10-22 15:50:05 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,44 +1,66 @@
 import os
 from time import sleep
 
-def s (t1,t2):
+r1=('/data/data/com.termux/files/home')
+r2 = ('cd')
+h1=r2,r1
+def t (t1,t2):
     tt1=sleep(t1)
     tt2=sleep(t2)
     return tt1,tt2
     
-def o (i):
+def s (i): 
     o1=os.system(i)
     return o1
+
+def rumah ():
+    o2 = s (r2)
+    return o2
     
 def update ():
-    os.system('clear')
+    s('clear')
+    rumah()
     print ('cek pembaruan paket')
-    s(5,2)
-    o('apt update')
-    s(2,2)
-    os.system('clear')
+    t(5,2)
+    s('apt update')
+    t(2,2)
+    s('clear')
+    print ('paket yang akan di tingkat kan ')
+    s('apt list --upgradable')
+    t(3,0)
     print('peningkatan paket ')
-    s(2,3)
-    o('apt upgrade  -y')
-    os.system('clear')
-    s(3,0)
+    t(2,3)
+    s('apt upgrade  -y')
+    s('clear')
+    t(3,0)
     print('pembaruan selesai ')
-    exit()
     
     
     
+#mulai program 
 print (f"{'SELAMAT DATANG':^45}")
-print (''' ini adalah script pembaruan firwer secara otomatis '''
+print (''' ini adalah script pembaruan firwer secara
+ otomatis apakah anda setuju'''
 )
 
 while True :
-    t = input('apakah anda setuju:  (y/n)')
-    if t == 'y':
+    T = input(' (y/n) : ')
+    if T == 'y':
+        print (f"{'MULAI':^45}")
+        update()
+    elif T == 'Y':
         print (f"{'MULAI':^45}")
         update()
         
-    elif t == 'n':
-        print ("{'perogram selesae ':^45}")
+    elif T == 'n':
+        print (f"{'perogram selesae ':^45}")
         exit()
-
+        
+    elif T == 'N':
+        print (f"{'perogram selesae ':^45}")
+        exit()
+        
+        
+    else :
+        print ('Pilhan salah ulangi lagi ')
 
